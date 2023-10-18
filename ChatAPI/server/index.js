c51c4c36c666c9d7cb0f48a466cfef3deecc0019
akimandarinas <dezavalai94@gmail.com>
2023-10-18 23:06:48 +0200

    servidor completo

--- ChatAPI/server/index.js
+++ ChatAPI/server/index.js
@@ -1,16 +1,34 @@
 import express from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io';
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3006;
 
-const app = express();
-app.use(logger('dev'));
+const api = express();
+const servidor = createServer(api)
+const io = new Server(servidor, {})
 
-app.get('/', function(req, res){
+io.on('connection',  (user) =>{
+    console.log('a user has connected!')
+
+    user.on('disconnect', () => {
+        console.log('an user has disconnected')
+    })
+
+    user.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+api.use(logger('dev'));
+
+api.get('/', function(req, res){
     res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.use(express.static("client"));
+api.use(express.static("client"));
 
-app.listen(port, () =>{
+servidor.listen(port, () =>{
     console.log('Server running on port 3006');
 });
